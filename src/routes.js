5274315d8b4785dd0a5346330a2271b2ec2a770f
Nathan Palatin <nath.palatin@gmail.com>
2024-02-20 10:49:45 -0300

    [chore] adicionado o metodo de deletar usuario e fazendo tratativa de rotas.

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { randomUUID } from 'node:crypto'
 
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const secretKey = 'Mudaromundo2024'
 
@@ -11,7 +12,7 @@ const database = new Database()
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -20,7 +21,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: async (req, res) => {
 
       const { name, email, password, avatar, username } = req.body
@@ -45,7 +46,20 @@ export const routes = [
 
       return res
         .writeHead(201)
-        .end(`Usuário criado com sucesso! ${token}`)
+        .end(`Usuário criado com sucesso!`)
     }
-  }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end(`Usuário removido com sucesso!`)
+
+    }
+  },
 ]
